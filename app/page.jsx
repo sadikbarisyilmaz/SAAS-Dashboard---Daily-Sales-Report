7bd1b4e8c9764b1df7aa66067b3c805f88444d38
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2024-05-15 18:09:19 +0300

    Fix: used &apos; instead of "'"

--- app/page.jsx
+++ app/page.jsx
@@ -7,7 +7,7 @@ export default function Home() {
       <Card className="grid gap-2 dark:bg-black bg-white dark:bg-opacity-40 bg-opacity-40 p-8 shadow-md dark:shadow-slate-700 h-full">
         <div className="text-center flex flex-col items-center justify-center max-w-2xl">
           <h1 className="drop-shadow-2xl px-4 py-8 text-4xl font-extrabold lg:text-4xl">
-            Welcome to Your Company's Dashboard !
+            Welcome to Your Company&apos;s Dashboard !
           </h1>
           <p className="leading-7 text-foreground/80">
             <Link className=" link" href="/dashboard">
