3ebf9e6873b6b2485ae8b103cf2de57c75284b72
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2024-05-15 19:01:35 +0300

    Fix: Removed unnecessary quotes

--- app/api/sale/route.js
+++ app/api/sale/route.js
@@ -1,23 +1,18 @@
 
 import { connectToDataBase } from "@/database";
 import Sale from "@/models/sales";
-import User from "@/models/user"; // Adjust the path as necessary
 import { NextResponse } from "next/server";
 
 const handler = async (req, res) => {
     await connectToDataBase();
     try {
         const request = await req.json()
-        // Hash the password
-        // Create a new user with the hashed password
         const sale = await Sale.insertMany(request);
-        // Respond with the created user (excluding the password)
         return NextResponse.json(sale, { status: 201 });
 
     } catch (error) {
-        console.error("Signup error:", error);
         return NextResponse.json(
-            { error: "User creation failed" },
+            { error: "Sale creation failed" },
             { status: 400 }
         );
     }
